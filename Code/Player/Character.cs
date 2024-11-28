17e4373d7ddbc991e0f068bcead348807a1cc7b1
pepeien <erickfcaldeira@gmail.com>
2024-11-28 17:14:38 -0300

    Introduced Player Character camera zoom on aim

---
+++ Code/Player/Character.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Player
+{
+	public sealed class Character : Component
+	{
+		[Property]
+		public PlayerController Controller { get; set; }
+
+		private float FieldOfView = -1.0f;
+
+		private const float ZOOM_COEFFICIENT = 0.35f;
+		private const float ZOOMED_IN_FOV = 45.0f;
+		private const float ZOOMED_OUT_FOV = 79.0f;
+
+		protected override void OnPreRender()
+		{
+			base.OnPreRender();
+
+			CameraComponent camera = Controller.Scene.Camera;
+
+			if (Input.Down("attack2"))
+			{
+				ZoomCameraIn(camera);
+
+				return;
+			}
+
+			ZoomCameraOut(camera);
+		}
+
+		private void ZoomCameraIn(CameraComponent camera)
+		{
+			if (camera == null)
+			{
+				return;
+			}
+
+			if (this.FieldOfView < 0.0f)
+			{
+				this.FieldOfView = camera.FieldOfView;
+			}
+
+			if (this.FieldOfView > ZOOMED_IN_FOV)
+			{
+				this.FieldOfView -= ZOOM_COEFFICIENT;
+			}
+			
+			camera.FieldOfView = this.FieldOfView;
+			camera.LocalTransform = camera.LocalTransform.Add(Vector3.Left * -2.0f, false);
+		}
+
+		private void ZoomCameraOut(CameraComponent camera)
+		{
+			if (this.FieldOfView < 0.0f)
+			{
+				this.FieldOfView = camera.FieldOfView;
+			}
+
+			if (this.FieldOfView < ZOOMED_OUT_FOV)
+			{
+				this.FieldOfView += ZOOM_COEFFICIENT;
+			}
+
+			camera.FieldOfView = this.FieldOfView;
+			camera.LocalTransform = camera.LocalTransform.Add(Vector3.Left * 2.0f, false);
+		}
+	}
+}
